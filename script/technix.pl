8fdd5538cae9cee8223993e687c95c14e65f056d
Sergei Mozhaisky <sergei.mozhaisky@gmail.com>
2013-12-16 16:31:18 +0000

    pergolf-10 v3

--- script/technix.pl
+++ script/technix.pl
@@ -1,2 +1,2 @@
-#!perl -anlp
-$|=($y,$x)=@F[4,5];@o=(-1,0,1,0);for$i(0..3){$_=$F[$i]eq'#'?next:"$o[$i] $o[3-$i]";last if($y&&abs($y+$o[$i])>abs$y)+($x&&abs($x+$o[3-$i])>abs$x)}
+#!perl -alp
+$|=($y,$x)=@F[4,5];@o=(-1,0,1,0);for$i(0..3){@p=@o[$i,3-$i];$_=$F[$i]eq'#'?next:"@p";last if($y&&abs($y+$p[0])>abs$y)+($x&&abs($x+$p[1])>abs$x)}
