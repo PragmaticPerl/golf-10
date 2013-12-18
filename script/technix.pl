618ccda9547a1699ed2b538b4296a7f3557817f0
Sergei Mozhaisky <sergei.mozhaisky@gmail.com>
2013-12-18 11:42:21 +0000

    perlgolf-10 v4

--- script/technix.pl
+++ script/technix.pl
@@ -1,2 +1,2 @@
 #!perl -alp
-$|=($y,$x)=@F[4,5];@o=(-1,0,1,0);for$i(0..3){@p=@o[$i,3-$i];$_=$F[$i]eq'#'?next:"@p";last if($y&&abs($y+$p[0])>abs$y)+($x&&abs($x+$p[1])>abs$x)}
+$|=@o=(-1,0,1,0);for$i(0..3){@p=@o[$i,3-$i];$_=$F[$i]eq'#'?next:"@p";$c=$d=0;map{$c+=$_&&abs($_+$p[$d])>abs;$d++}@F[4,5];last if$c}
