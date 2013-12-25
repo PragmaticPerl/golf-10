6d67c87acf9585b609d17fcd6f9de82dc48aacfd
Sergei Mozhaisky <sergei.mozhaisky@gmail.com>
2013-12-25 12:15:17 +0000

    perlgolf-10 v6

--- script/technix.pl
+++ script/technix.pl
@@ -1,2 +1,2 @@
 #!perl -alp
-$|=@o=(-1,0,1,0);for$i(0..3){@p=@o[$i,3-$i];$_=$F[$i]eq'#'?next:"@p";last if map{abs($_+pop@p)>abs&&$_?1:()}@F[5,4]}
+$|=@o=(-1,0,1,0);for$i(0..3){@p=@o[$i,3-$i];$_=$F[$i]eq'#'?next:"@p";last if grep{$_*pop@p>0}@F[5,4]}
